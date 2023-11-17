bccb719807f9b83ce2131acdac064f381f55d940
fargito <fr.farge96@gmail.com>
2023-11-17 14:33:26 +0100

    feat: bootstrap io + empty tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+fn main() -> io::Result<()> {
+    let mut first_line = String::new();
+    let mut second_line = String::new();
+    io::stdin().read_line(&mut first_line)?;
+    io::stdin().read_line(&mut second_line)?;
+
+    let res = handle(first_line, second_line);
+
+    println!("{res}");
+
+    Ok(())
+}
+
+fn handle(first_line: String, second_line: String) -> String {
+    "toto".to_string()
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_1() {}
+
+    #[test]
+    fn test_2() {}
+
+    #[test]
+    fn test_3() {}
 }
